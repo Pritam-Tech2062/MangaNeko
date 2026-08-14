484671ee84687f238a670cf7c887968c8e2a9da4
Lui-coder01 <shirosagilui7@gmail.com>
2026-08-14 13:15:37 +0530

    Fix production backend URL

--- MangaFrontend/src/api.js
+++ MangaFrontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'https://manganeko-2.onrender.com/api',
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
 });
 
 // Search manga by title
